e1ba89a5fbc56e2da3e5ffc8aff2cb927537c5f2
JY-goodee <an9979sk@naver.com>
2021-02-27 23:44:11 +0900

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by 윤진영 on 2021/02/27.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
